03d421e56b76c97b3a1c169bdb24fee56a95af98
zhysunny <1065300897@qq.com>
2019-11-15 00:43:33 +0800

    netty

---
+++ zhysunny-network/src/main/java/com/zhysunny/network/netty/NettyServer.java
@@ -0,0 +1,62 @@
+package com.zhysunny.network.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author 章云
+ * @date 2019/11/14 22:45
+ */
+public class NettyServer extends SimpleChannelInboundHandler<ByteBuf> {
+
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+        // 创建mainReactor
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        // 创建工作线程组
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        final ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+        // 组装NioEventLoopGroup
+        .group(boosGroup, workerGroup)
+        // 设置channel类型为NIO类型
+        .channel(NioServerSocketChannel.class)
+        // 设置连接配置参数
+        .option(ChannelOption.SO_BACKLOG, 1024)
+        .childOption(ChannelOption.SO_KEEPALIVE, true)
+        .childOption(ChannelOption.TCP_NODELAY, true)
+        // 配置入站、出站事件handler
+        .childHandler(new ChannelInitializer<NioSocketChannel>() {
+            @Override
+            protected void initChannel(NioSocketChannel ch) {
+                // 配置入站、出站事件channel
+                ch.pipeline().addLast(new NettyServer());
+            }
+        });
+        // 绑定端口
+        int port = 9999;
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println(new Date() + ": 端口[" + port + "]绑定成功!");
+            } else {
+                System.err.println("端口[" + port + "]绑定失败!");
+            }
+        });
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println(count.addAndGet(1) + byteBuf.toString(Charset.forName("UTF-8")));
+    }
+}
