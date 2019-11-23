6476cbfeb4ad32dd7dcfe21e961701d55e807864
zhangyun <0049003310@znv.com>
2019-11-23 15:41:42 +0800

    排序

---
+++ zhysunny-java/src/main/java/com/zhysunny/java/structure/sort/BubbleSort.java
@@ -0,0 +1,41 @@
+package com.zhysunny.java.structure.sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ * @author 章云
+ * @date 2019/11/23 15:10
+ */
+public class BubbleSort {
+
+    private int[] array;
+    private int[] sort;
+
+    public BubbleSort(int[] array) {
+        this.array = array;
+        copy();
+        sort();
+    }
+
+    private void copy() {
+        this.sort = Arrays.copyOf(array, array.length);
+    }
+
+    private void sort() {
+        if (sort.length > 1) {
+            for (int j = 0; j < sort.length - 1; j++) {
+                int max = sort[0];
+                for (int i = 0; i < sort.length - 1 - j; i++) {
+                    if (max < sort[i + 1]) {
+                        max = sort[i + 1];
+                    } else {
+                        sort[i] = sort[i + 1];
+                        sort[i + 1] = max;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ zhysunny-java/src/test/java/com/zhysunny/java/structure/sort/BubbleSortTest.java
@@ -0,0 +1,81 @@
+package com.zhysunny.java.structure.sort;
+
+import static com.zhysunny.java.structure.sort.BubbleSort.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.Arrays;
+
+/**
+ * BubbleSort Test.
+ * @author 章云
+ * @date 2019/11/23 15:10
+ */
+public class BubbleSortTest {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        System.out.println("Test BubbleSort Class Start...");
+    }
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        System.out.println("Test BubbleSort Class End...");
+    }
+
+    @Test
+    public void testSort1() throws Exception {
+        int[] array = { 3 };
+        sort(array);
+        assertEquals(array[0], 3);
+    }
+
+    @Test
+    public void testSort2() throws Exception {
+        int[] array = { 3, 2 };
+        sort(array);
+        assertEquals(array[0], 2);
+        assertEquals(array[1], 3);
+    }
+
+    @Test
+    public void testSort3() throws Exception {
+        int[] array = { 3, 2, 1, 6, 2 };
+        sort(array);
+        assertEquals(array[0], 1);
+        assertEquals(array[1], 2);
+        assertEquals(array[2], 2);
+        assertEquals(array[3], 3);
+        assertEquals(array[4], 6);
+    }
+
+    @Test
+    public void testSort4() throws Exception {
+        int[] array = { 10, 8, 12, 6, 15 };
+        sort(array);
+        assertEquals(array[0], 6);
+        assertEquals(array[1], 8);
+        assertEquals(array[2], 10);
+        assertEquals(array[3], 12);
+        assertEquals(array[4], 15);
+    }
+
+    /**
+     * @param array 排序前
+     * @param sort  排序后
+     */
+    private void test(int[] array, int[] sort) {
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            assertEquals(array[i], sort[i]);
+        }
+    }
+
+}
