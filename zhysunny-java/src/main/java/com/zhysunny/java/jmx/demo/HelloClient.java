de10476776b31f211ad85a4263e52778c85a3c97
zhangyun <0049003310@znv.com>
2019-12-24 10:02:39 +0800

    jmx

---
+++ zhysunny-java/src/main/java/com/zhysunny/java/jmx/demo/HelloClient.java
@@ -0,0 +1,51 @@
+package com.zhysunny.java.jmx.demo;
+
+import javax.management.*;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import java.util.Arrays;
+
+/**
+ * @author 章云
+ * @date 2019/12/24 9:54
+ */
+public class HelloClient {
+
+    public static void main(String[] args) throws Exception {
+        String jmxURL = "service:jmx:rmi:///jndi/rmi://127.0.0.1:1000/jmxrmi";
+        JMXServiceURL serviceURL = new JMXServiceURL(jmxURL);
+        JMXConnector connector = JMXConnectorFactory.connect(serviceURL);
+        MBeanServerConnection mbsc = connector.getMBeanServerConnection();
+        // MBean名称
+        ObjectName objectName = new ObjectName("com.zhysunny.java.jmx.demo:name=HelloServer");
+        // String
+        System.out.println("name:" + mbsc.getAttribute(objectName, "Name"));
+        // long
+        System.out.println("time:" + mbsc.getAttribute(objectName, "Time"));
+        // 1个操作
+        MBeanInfo mBeanInfo = mbsc.getMBeanInfo(objectName);
+        String className = mBeanInfo.getClassName();
+        System.out.println("MBean类名：" + className);
+        String description = mBeanInfo.getDescription();
+        System.out.println("描述：" + description);
+        MBeanConstructorInfo[] constructors = mBeanInfo.getConstructors();
+        System.out.println("构造器个数：" + constructors.length);
+        for (MBeanConstructorInfo constructor : constructors) {
+            System.out.println(constructor);
+        }
+        Descriptor descriptor = mBeanInfo.getDescriptor();
+        System.out.println("描述信息：" + descriptor);
+        MBeanNotificationInfo[] notifications = mBeanInfo.getNotifications();
+        System.out.println("通知个数：" + notifications.length);
+        for (MBeanNotificationInfo notification : notifications) {
+            System.out.println(notification);
+        }
+        MBeanOperationInfo[] operations = mBeanInfo.getOperations();
+        System.out.println("操作个数：" + operations.length);
+        for (MBeanOperationInfo operation : operations) {
+            System.out.println(operation);
+        }
+    }
+
+}
--- zhysunny-java/src/main/java/com/zhysunny/java/jmx/demo/HelloServer.java
+++ zhysunny-java/src/main/java/com/zhysunny/java/jmx/demo/HelloServer.java
@@ -2,18 +2,13 @@ package com.zhysunny.java.jmx.demo;
 
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
-import javax.management.remote.JMXConnectorServer;
-import javax.management.remote.JMXConnectorServerFactory;
-import javax.management.remote.JMXServiceURL;
 import java.lang.management.ManagementFactory;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 /**
  * @author 章云
  * @date 2019/10/29 15:51
  */
-public class HelloAgent {
+public class HelloServer {
 
     public static void main(String[] args) throws Exception {
         // 下面这种方式不能再JConsole中使用
@@ -22,12 +17,19 @@ public class HelloAgent {
         // 调用MBean的方法，然后生产我们的资源的一个对象。
         MBeanServer server = ManagementFactory.getPlatformMBeanServer();
         //为MBean（下面的new Hello()）创建ObjectName实例
-        ObjectName helloName = new ObjectName("com.zhysunny.java.jmx.demo:name=HelloAgent");
+        ObjectName helloName = new ObjectName("com.zhysunny.java.jmx.demo:name=HelloServer");
         // 将new Hello()这个对象注册到MBeanServer上去
         Hello hello = new Hello();
         hello.setName("zhysunny");
         server.registerMBean(hello, helloName);
+
         Thread.sleep(1000000000);
+
+        // 执行参数必须加上
+        //        -Djava.rmi.server.hostname=127.0.0.127
+        //        -Dcom.sun.management.jmxremote.port=1000
+        //        -Dcom.sun.management.jmxremote.ssl=false
+        //        -Dcom.sun.management.jmxremote.authenticate=false
     }
 
 }
